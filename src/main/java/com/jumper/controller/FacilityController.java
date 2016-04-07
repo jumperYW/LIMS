185d5215844f4222faa93e4ff4555f29fa37f14b
jumperYW <187008315322163.com>
2016-04-07 21:53:13 +0800

    修改乱码

--- src/main/java/com/jumper/controller/FacilityController.java
+++ src/main/java/com/jumper/controller/FacilityController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.jumper.dto.FacilityDto;
-import com.jumper.dto.UserDto;
 import com.jumper.entity.TFacility;
-import com.jumper.entity.TUser;
 import com.jumper.service.FacilityService;
 
 @Controller

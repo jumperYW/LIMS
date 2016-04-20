f3b0826a9a224c42643c6afe0aa6597b96f3b84f
jumperYW <540769218@qq.com>
2016-04-20 21:18:10 +0800

    增加状态查询

--- src/main/java/com/jumper/controller/FacilityController.java
+++ src/main/java/com/jumper/controller/FacilityController.java
@@ -107,12 +107,15 @@ public class FacilityController {
 		if(mapjson != null){
 			map = JSON.parseObject(mapjson,Map.class);
 		}
+		int state = Integer.parseInt(map.get("state"));
+		map.remove("state");
 		List<TFacility> facilies = facilityService.findPageByCriteria(pageNo, pageSize, map);
 		logger.info("查询成功！TFacilities:"+JSON.toJSONString(facilies));
 		List<TFacility> facs = new ArrayList<TFacility>();
 		for(TFacility facility : facilies){
 			//facDto = getDtoFromFacility(facility);
-			facs.add(facility);
+			if(facility.getState()==state)
+				facs.add(facility);
 		}
 		logger.info("Facs:"+JSON.toJSONString(facs));
 		return JSON.toJSONString(facs);
